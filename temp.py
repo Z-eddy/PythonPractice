a45f3f7b0534fa1d24b98eef90df64987db48b2a
zr <576172400@qq.com>
2020-06-08 17:02:46 +0800

    pop弹出元素
    默认弹出最后元素,可以指定弹出的索引

--- temp.py
+++ temp.py
@@ -1,8 +1,6 @@
 tempList=["test1","test2",1,2,3,"test3"]
 print(tempList)
-tempList.append("appendElement")
-print(tempList)
-tempList.insert(1,"insertData")
-print(tempList)
-del tempList[3]
-print(tempList)
+ele=tempList.pop()#默认弹出最后元素
+print(tempList,ele)
+ele=tempList.pop(3)
+print(tempList,ele)
