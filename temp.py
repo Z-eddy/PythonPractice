6137fab40bc97ae29f4e223d7ae40156f3ae9d88
Z-eddy <576172400@qq.com>
2020-06-08 23:51:54 +0800

    只打印key

--- temp.py
+++ temp.py
@@ -1,5 +1,5 @@
 keyValue={"key":"theKey","val0":123,"val1":"the456"}
 keyValue["add"]=12.59
 
-for key,value in keyValue.items():
-    print(key,value)
+for key in keyValue.keys(): #.values()
+    print(key)
