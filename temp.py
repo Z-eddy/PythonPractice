641fbd2be6e4f65773b42b0550a37f0aa48d8a08
zr <576172400@qq.com>
2020-06-08 15:19:26 +0800

    str转换到字符串

--- temp.py
+++ temp.py
@@ -1,8 +1,3 @@
-message="     hello 'world'\t      "
-#生成临时亮而非更改自身
-tempDelRightMes=message.rstrip()#去掉右边空白
-tempDelLeftMes=message.lstrip()#去掉左边空白
-tempDelWhiteMes=message.strip()#去掉两边空白
-print(tempDelRightMes)
-print(tempDelLeftMes)
-print(tempDelWhiteMes)
+num=12.53
+message="age:"+str(12.53)
+print(message)
