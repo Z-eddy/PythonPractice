2c8db0b9a2711b8989ed3651645015592259c7a1
Z-eddy <576172400@qq.com>
2020-06-08 23:56:45 +0800

    set创建不重复的集合

--- temp.py
+++ temp.py
@@ -1,5 +1,5 @@
-keyValue={"key":"theKey","val0":123,"val1":"the456"}
+keyValue={"key":"theKey","val0":123,"val1":123}
 keyValue["add"]=12.59
 
-for key in keyValue:    #默认只访问键名
+for key in set(keyValue.values()):    #默认只访问键名
     print(key)
