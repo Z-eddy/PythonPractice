224a453ac3b9f5691142906633dadc10fae32f9d
zr <576172400@qq.com>
2020-06-08 14:46:08 +0800

    title,upper,lower
    首字母大写,全部大写,全部小写

--- temp.py
+++ temp.py
@@ -1,2 +1,5 @@
-message="hello 'world'"
-print(message)
+message="hello 'world'\t"
+tempTitle=message.title()   #首字母大写
+tempUpper=message.upper()   #全部大写
+tempLower=message.lower()   #全部小写
+print(message,tempTitle,tempUpper,tempLower)
