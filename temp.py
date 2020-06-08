7d250211a94e8414b6c780d24b2a71556a8f7e9b
zr <576172400@qq.com>
2020-06-08 17:00:09 +0800

    append insert del增、插入、删除

--- temp.py
+++ temp.py
@@ -1,2 +1,8 @@
 tempList=["test1","test2",1,2,3,"test3"]
-print(tempList[-2])
+print(tempList)
+tempList.append("appendElement")
+print(tempList)
+tempList.insert(1,"insertData")
+print(tempList)
+del tempList[3]
+print(tempList)
