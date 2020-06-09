94bd439d6ec25e529d2f6b076371973b498bd8ae
Z-eddy <576172400@qq.com>
2020-06-09 22:35:35 +0800

    模块函数指定别名

--- temp.py
+++ temp.py
@@ -1,3 +1,3 @@
-from theModule import dealTuple
+from theModule import dealTuple as foo
 
-dealTuple("test", 123, nameA=["aa", 1.86, 'bb'], nameB=123115)
+foo("test", 123, nameA=["aa", 1.86, 'bb'], nameB=123115)
