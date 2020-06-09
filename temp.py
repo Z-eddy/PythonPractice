5b4956895b38a241d3c420a076be94ec111d8819
Z-eddy <576172400@qq.com>
2020-06-09 20:50:38 +0800

    input输入1个参数

--- temp.py
+++ temp.py
@@ -1,13 +1,4 @@
-li = []
-for i in range(2, 10):
-    print(i)
-    tempItem = {"n0": i+10, "n1": i+11}
-    li.append(tempItem)
-
-print(li)
-
-for item in li[:3]:
-    item["no"]=88
-    item["n1"]=99
-
+li=[]
+mes = input("输入:")
+li.append(mes)
 print(li)
