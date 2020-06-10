637e4c2f798846f1e3e7e466c78bad1bc40f5f46
zr <576172400@qq.com>
2020-06-10 18:01:58 +0800

    文件按行读取

--- temp.py
+++ temp.py
@@ -1,3 +1,5 @@
-with open("piDigits.txt") as piObj:
-    data=piObj.read()
-    print(data)
+with open("./temp/piDigits.txt") as piObj:
+    i=0
+    for line in piObj:  #piObj.readlines(0) 从0行开始读取
+        print(line.strip()+" line:"+str(i))
+        i+=1
