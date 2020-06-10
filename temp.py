4f3dce35d7c60c037b6c0099ed6b8a504867987c
zr <576172400@qq.com>
2020-06-10 14:20:04 +0800

    指定调用模块类

--- temp.py
+++ temp.py
@@ -1,16 +1,6 @@
-class Dog():
-    def __init__(self, name, age):
-        self.name_ = name
-        self.age_ = age
+from theModule import Dog as Pet
 
-    def sit(self):
-        print(self.name_+" is sit")
-
-    def petAge(self):
-        print("the age is:"+str(self.age_))
-
-
-myDog = Dog("nameA",3)
+myDog =Pet("nameA",3)
 print(myDog.name_,myDog.age_)
 myDog.sit()
 myDog.petAge()
--- theModule.py
+++ theModule.py
@@ -1,2 +1,10 @@
-def dealTuple(numA,numB,**theMap):
-    print(numA,numB,theMap)
+class Dog():
+    def __init__(self, name, age):
+        self.name_ = name
+        self.age_ = age
+
+    def sit(self):
+        print(self.name_+" is sit")
+
+    def petAge(self):
+        print("the age is:"+str(self.age_))
