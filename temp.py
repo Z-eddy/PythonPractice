8ee563fc89228d43b1d0223aa1ebad94cd952134
zr <576172400@qq.com>
2020-06-10 17:51:51 +0800

    简单文件读取
    with自动使用close文件,open用于打开,read返回文档字符串内容

--- temp.py
+++ temp.py
@@ -1,18 +1,3 @@
-normalDict={}
-normalDict["cda"]="theC"
-normalDict["abd"]="theA"
-normalDict["b"]="theB"
-normalDict["num"]=17.8
-# print(normalDict)
-for key,val in normalDict.items():
-    print(key,val)
-
-from collections import OrderedDict
-ordDict=OrderedDict()
-ordDict["cda"]="theC"
-ordDict["abd"]="theA"
-ordDict["b"]="theB"
-ordDict["num"]=17.8
-# print(ordDict)
-for key,val in ordDict.items():
-    print(key,val)
+with open("piDigits.txt") as piObj:
+    data=piObj.read()
+    print(data)
