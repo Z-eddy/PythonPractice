5bcbe09c405c8e1f0b253485f038a18454562096
zr <576172400@qq.com>
2020-06-10 15:16:07 +0800

    测试不通过形参传值的属性值,仍然是公有

--- inheritTemp.py
+++ inheritTemp.py
@@ -4,5 +4,6 @@ class SpottyDog(Dog):
     def __init__(self,name,age,spottyNum):
         super().__init__(name,age)
         self.spottyNum_=spottyNum
+        self.privateTest=12.98  #仍然是公有
     def spottyN(self):
         return self.spottyNum_
