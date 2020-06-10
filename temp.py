ae6842da5aecaf0bf4a780816fdb826db8880a49
Z-eddy <576172400@qq.com>
2020-06-11 00:11:35 +0800

    string.split字符串分解成列表

--- temp.py
+++ temp.py
@@ -1,11 +1,3 @@
-n0=5
-n1=0
-try:
-    result=n0/n1
-    print("no pass?")#自动跳过了！
-    with open("other") as fileObj:#自动跳过了！
-        fileObj.read()
-except ZeroDivisionError:
-    print("devide 0!")
-else:
-    print("other error")
+sTotal="this is a word"
+wordLi=sTotal.split()
+print(wordLi)
