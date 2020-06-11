879d818707295e4a63c20456269bb31c2d84c6a5
zr <576172400@qq.com>
2020-06-11 10:48:51 +0800

    字符串操作

--- temp.py
+++ temp.py
@@ -1,3 +1,20 @@
 sTotal="this is a word"
-wordLi=sTotal.split()
-print(wordLi)
+print(sTotal[::2])#截取语法:变量[头下标:尾下标:步长]
+print(sTotal[0:-1])
+print(sTotal[2:5])
+print(sTotal[0])#一个字符
+print(sTotal[2:])
+print(sTotal*3)
+print(sTotal+"你好")
+print(r"hello\n")
+
+"""
+ti sawr
+this is a wor
+is
+t
+is is a word
+this is a wordthis is a wordthis is a word
+this is a word你好
+hello\n
+"""
