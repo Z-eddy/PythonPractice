dcb6ff83afc73087df8ae7c27d33d5647c66614d
zr <576172400@qq.com>
2020-06-11 12:12:46 +0800

    set元组间计算

--- temp.py
+++ temp.py
@@ -1,10 +1,6 @@
-li=["abawwefsdfe","test"]
-a=set(li)
-print(a)
-b=set("abasdeff")
-print(b)
-
-"""
-{'test', 'abawwefsdfe'}
-{'a', 'f', 'e', 'b', 'd', 's'}
-"""
+a=set("abcde")
+b=set("cdefg")
+print(a-b)#差集,a有b没有
+print(a|b)#并集
+print(a&b)#交集
+print(a^b)#特有值集合
