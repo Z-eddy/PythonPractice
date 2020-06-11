082fa021176cf275e1ddf833b1f5c43c8620f861
zr <576172400@qq.com>
2020-06-11 11:55:58 +0800

    构造一个元素的元组需要加,

--- temp.py
+++ temp.py
@@ -1,9 +1,4 @@
-def reverString(s):
-    words=s.split(" ")#按照空格分离word
-    words=words[-1::-1]
-    outSentence=" "#也可以和下面直接合并
-    outSentence = outSentence.join(words)
-    return outSentence
-
-s="i like the world"
-print(reverString(s))
+tup1=()
+tup2=(20,)#构造一个元素的元组需要加,
+tup3=("abc",12,11,"bcd")
+print(tup3+tup2)
