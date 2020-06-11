d1e28c3967bf2711602892163a1dd33b8e489e66
zr <576172400@qq.com>
2020-06-11 11:34:07 +0800

    * + 列表间操作

--- temp.py
+++ temp.py
@@ -1,2 +1,15 @@
-sTotal="this is a word"
-print(type(sTotal))
+li=["aa","b","cc","dd"]
+tynyLi=["ee","f"]
+print(li[1])
+print(li[0:2])
+print(li[1:])
+print(tynyLi*3)
+print(li+tynyLi)
+
+"""
+b
+['aa', 'b']
+['b', 'cc', 'dd']
+['ee', 'f', 'ee', 'f', 'ee', 'f']
+['aa', 'b', 'cc', 'dd', 'ee', 'f']
+"""
