cbe4a35203712c682b2d5d6795abdfe1eaf5e2dc
zr <576172400@qq.com>
2020-06-11 17:23:40 +0800

    unittest测试函数必须以test开头才会自动运行、测试

--- temp.py
+++ temp.py
@@ -2,8 +2,8 @@ from theModule import getFormattedName
 import unittest
 
 class nameTest(unittest.TestCase):  #通过继承TestCase
-    def testFirstLastName(self):
+    def testFirstLastName(self):    #只有以test开头才会运行
         name=getFormattedName("jian","que")
-        self.assertEqual(name,"Jian Que")
+        self.assertEqual(name,"ian Que")
 
 unittest.main()
