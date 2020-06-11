40cdfbe02af3e189db671f88d16fa7d9d43d24af
Z-eddy <576172400@qq.com>
2020-06-11 23:15:37 +0800

    unittest的setUP初始化资源
    将待测试的类作为测试类的属性进行初始化、储存

--- temp.py
+++ temp.py
@@ -2,9 +2,11 @@ import unittest
 from theModule import Dog
 
 class TestDog(unittest.TestCase):
+    def setUp(self):
+        self.myPet_=Dog("nameA",2)
+        self.li_=["nameB","nameA"]
+
     def testName(self):
-        myPet=Dog("nameA",2)
-        li=["nameB","nameA"]
-        self.assertIn(myPet.getName(),li)
+        self.assertIn(self.myPet_.getName(),self.li_)
 
 unittest.main()
