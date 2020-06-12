4a3b3b4dfa1fe7a551d6e64d3f6399c255349a20
zr <576172400@qq.com>
2020-06-12 17:43:29 +0800

    PyQt5简单窗口

---
+++ Project/PyQt5/TestPyQt/SimpleMainWindow.py
@@ -0,0 +1,11 @@
+﻿import sys
+from PyQt5.QtWidgets import QApplication,QMainWindow
+from mainWindow import Ui_MainWindow
+
+if __name__ == "__main__":
+    app=QApplication(sys.argv)
+    w=QMainWindow()
+    ui=Ui_MainWindow()
+    ui.setupUi(w)
+    w.show()
+    sys.exit(app.exec())
--- temp.py
+++ temp.py
@@ -3,7 +3,11 @@ import unittest
 
 class nameTest(unittest.TestCase):  #通过继承TestCase
     def testFirstLastName(self):    #只有以test开头才会运行
-        name=getFormattedName("jian","que")
+        name=getFormattedName("jian")
         self.assertEqual(name,"ian Que")
+
+    def testFirstLastName(self):    #只有以test开头才会运行
+        name=getFormattedName("jian","que")
+        self.assertEqual(name,"Jian Que")
 
 unittest.main()
