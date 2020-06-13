5537d0692ded86975515431d2e181d8c782f443d
Z-eddy <576172400@qq.com>
2020-06-13 21:54:14 +0800

    get setdefault字典方法
    保证有值

--- temp.py
+++ temp.py
@@ -1,13 +1,5 @@
-import unittest
-from theModule import Dog
-
-class nameTest(unittest.TestCase):  #通过继承TestCase
-    def testFirstLastName(self):    #只有以test开头才会运行
-        name=getFormattedName("jian")
-        self.assertEqual(name,"ian Que")
-
-    def testFirstLastName(self):    #只有以test开头才会运行
-        name=getFormattedName("jian","que")
-        self.assertEqual(name,"Jian Que")
-
-unittest.main()
+m = {"key0": 123, "key1": "test"}
+va0 = m.get("key2", 88)  # 当没有的时候使用默认值88
+print(m)
+m.setdefault("key3", "defaultValue")  # 有则不生效,无则生效
+print(m)
