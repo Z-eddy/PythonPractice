51ea68b25534ec9139f786c3defe170b4628a52d
zr <576172400@qq.com>
2020-06-16 16:35:21 +0800

    re.DOTALL帮助.*匹配一切字符

--- temp.py
+++ temp.py
@@ -1,11 +1,11 @@
 import re
 #构建检测的规则
-ruleStr=r"[0-9]+"
-ruleObj=re.compile(ruleStr)
+ruleStr=r".*"
+ruleObj=re.compile(ruleStr,re.DOTALL)   #编译时加入re.DOTALL可以匹配一切字符,否则换行符无法匹配到
 #具体检测的值
-message="the phone is (028)1234567."
-mo=ruleObj.findall(message)
+message="the phone is \n(028)1234567."
+mo=ruleObj.search(message)
 if(mo):
-    print(mo)
+    print(mo.group())
 else:
     print("can't find")
