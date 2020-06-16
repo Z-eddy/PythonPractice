623bcaaa1278738de807e1e69de52ee5b3332e19
zr <576172400@qq.com>
2020-06-16 10:16:39 +0800

    利用字典(键值对)对字符计数

--- temp.py
+++ temp.py
@@ -1,5 +1,8 @@
-m = {"key0": 123, "key1": "test"}
-va0 = m.get("key2", 88)  # 当没有的时候使用默认值88
-print(m)
-m.setdefault("key3", "defaultValue")  # 有则不生效,无则生效
-print(m)
+message="i have test this."
+count={}
+for char in message:
+    count.setdefault(char,0)
+    count[char]+=1
+
+for key,val in count.items():
+    print(key,val)
