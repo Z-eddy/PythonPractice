df65c6d4448806a0112e4454c7f27827d99cd933
zr <576172400@qq.com>
2020-06-16 15:01:51 +0800

    ()查找指定的值

--- temp.py
+++ temp.py
@@ -1,6 +1,11 @@
 import re
-rule=re.compile("^[a-z]*.*\d{3}")
-message="i have the13456 num 789012abc"
-result=rule.search(message)
-if(result):
-    print(result.group())
+#构建检测的规则
+ruleStr=r"[\(]?(\d{3})[\)]?[._-]?(\d{7})"
+ruleObj=re.compile(ruleStr)
+#具体检测的值
+message="the phone is (028)1234567."
+mo=ruleObj.search(message)
+if(mo):
+    print(mo.groups())  #.groups返回python的元组
+else:
+    print("can't find")
