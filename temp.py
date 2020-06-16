58c666af1531ac5a2a3e35b770a00d3906e6d82e
zr <576172400@qq.com>
2020-06-16 15:32:13 +0800

    .findall找到所有匹配,直接返回各匹配项的列表

--- temp.py
+++ temp.py
@@ -1,11 +1,11 @@
 import re
 #构建检测的规则
-ruleStr=r"[\(]?(\d{3})[\)]?[._-]?(\d{7})"
+ruleStr=r"[0-9]+"
 ruleObj=re.compile(ruleStr)
 #具体检测的值
 message="the phone is (028)1234567."
-mo=ruleObj.search(message)
+mo=ruleObj.findall(message)
 if(mo):
-    print(mo.groups())  #.groups返回python的元组
+    print(mo)
 else:
     print("can't find")
