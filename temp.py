7e816da9662c8ff43cd49b5d399cea0d49a6d542
zr <576172400@qq.com>
2020-06-16 11:47:28 +0800

    strip指定想要头尾去除的字符集

--- temp.py
+++ temp.py
@@ -1,4 +1,2 @@
-message = "abc"
-print(message.rjust(10, "_")+"end")  # 向右挪动10个字符,默认为空格 _______abcend
-print(message.ljust(15, "*")+"end")  # 向左挪动15个字符,默认为空格 abc************end
-print(message.center(20, "="))  # ========abc=========
+message = "beefghabcaseixoefbgef"
+print(message.strip("beg")) #删除"beg"中的任意出现在首位的字符
