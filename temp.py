8ce32404f3ecd88a4083baefe16efd0dd9ba4a08
zr <576172400@qq.com>
2020-06-16 11:32:38 +0800

    rjust,ljust,center填充字符

--- temp.py
+++ temp.py
@@ -1,5 +1,4 @@
-message=",".join(["i","have","test"])   #表示连接字符
-print(message)
-strli=message.split(",")    #以，切割划分为列表
-print(strli)
-#i,have,test
+message = "abc"
+print(message.rjust(10, "_")+"end")  # 向右挪动10个字符,默认为空格 _______abcend
+print(message.ljust(15, "*")+"end")  # 向左挪动15个字符,默认为空格 abc************end
+print(message.center(20, "="))  # ========abc=========
