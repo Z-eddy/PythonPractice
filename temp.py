70364773a8f01a0a7dbad45c00478b3431416053
Z-eddy <576172400@qq.com>
2020-07-06 22:39:30 +0800

    sys.exit()强制退出程序
    Ctrl+C手动退出无限循环

--- temp.py
+++ temp.py
@@ -1,8 +1,9 @@
 import random
+import sys
+rVal=0
+for i in range(100):
+    rVal=random.randint(0,100)
+    if rVal==98:
+        sys.exit()
 
-li = [1, 2, 3, 4, 51]
-if li:
-    print(10,5,8,len(li), end="\t", sep=":")
-    print("end")
-else:
-    print(0)
+print("end")
