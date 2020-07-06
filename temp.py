ab95a28610d26458d9079a4f88ef7f0432e19396
Z-eddy <576172400@qq.com>
2020-07-06 22:01:25 +0800

    print打印多个参数,并设定分隔符、结束符

--- temp.py
+++ temp.py
@@ -1,8 +1,8 @@
 import random
 
-li = [1, 2, 3]
+li = [1, 2, 3, 4, 51]
 if li:
-    print(1, len(li), end="\t", sep=":")
+    print(10,5,8,len(li), end="\t", sep=":")
     print("end")
 else:
     print(0)
