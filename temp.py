cca8fc51fac292f69303503c3ecdc1e7fd1af11b
Z-eddy <576172400@qq.com>
2020-07-16 23:53:53 +0800

    批量更改后缀名

--- temp.py
+++ temp.py
@@ -1,2 +1,10 @@
-li=[1,2,3]
-print(len(li))
+import os
+
+dirPath=r"D:\各种学习\语言\人工智能\万门大学\Python版"
+# dirPath = r"C:\Users\Administrator\Desktop\temp\测试文件"
+files = os.listdir(dirPath)
+for fileName in files:
+    index = fileName.find(".ts")
+    if index == -1:
+        newName = fileName+".ts"
+        os.rename(dirPath+"\\"+fileName, dirPath+"\\"+newName)
