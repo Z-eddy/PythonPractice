3d656c16c04318f822e51d19750fe5e24530a31e
赵睿 <zrendless@foxmail.com>
2020-10-18 17:41:32 +0800

    后缀名批量修改、文件复制到指定路径

---
+++ 小工具/文件复制到指定路径.py
@@ -0,0 +1,48 @@
+import os
+import shutil
+class  SearchFile(object):
+
+    def __init__(self,path='.'):
+        self._path=path
+        self.abspath=os.path.abspath(self._path) # 默认当前目录
+        print(self.abspath)
+
+    def findfile(self,keyword,root):
+        filelist=[]
+        for root,dirs,files in os.walk(root):
+            for name in files:
+                filelist.append(os.path.join(root, name))
+        print('...........................................')
+        dstFiles=[]
+        for i in filelist:            
+            if os.path.isfile(i):
+                if keyword in os.path.split(i)[1]:
+                    dstFiles.append(i)
+                    # print(i)    # 绝对路径
+        return dstFiles
+
+    def __call__(self):
+        root = self.abspath   # 把当前工作目录作为工作目录
+        # keyword = input('the keyword you want to find:')
+        keyword = "Ring"
+        if(keyword!=""):
+           files=self.findfile(keyword, root)  # 查找带指定字符的文件
+        #    print(files)
+           for file in files:
+                self.copyFileTo(file,root+"\\ring\\")
+
+    def copyFileTo(self,file,path):
+        print(file)
+        fileName=file.split("\\")
+        fileName=fileName[len(fileName)-1]
+        postfix=fileName.split(".")
+        postfix= postfix[len(postfix)-1]
+        if(postfix== "jpg" or postfix=="png"):
+            newPath=path+fileName
+            shutil.copy(file,newPath)
+
+
+
+if __name__ == '__main__':
+    search = SearchFile()
+    search()
