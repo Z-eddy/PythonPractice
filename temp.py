30146bc9d0cfc2e9c008a40d54fa4e404295bcdc
Zr <zrendless@foxmail.com>
2023-11-07 16:29:25 +0800

    feat: 使用 f"{}" 嵌入变量进行字符串输出

--- temp.py
+++ temp.py
@@ -1,10 +1,4 @@
-import os
-
-dirPath=r"D:\各种学习\语言\人工智能\万门大学\Python版"
-# dirPath = r"C:\Users\Administrator\Desktop\temp\测试文件"
-files = os.listdir(dirPath)
-for fileName in files:
-    index = fileName.find(".ts")
-    if index == -1:
-        newName = fileName+".ts"
-        os.rename(dirPath+"\\"+fileName, dirPath+"\\"+newName)
+valA=12
+valB="ab"
+rawStr=f"{valA}:{valB}"
+print(rawStr)
