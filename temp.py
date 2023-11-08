b722ea44b8e38602bcd57c56c09328e42f08e915
Zr <zrendless@foxmail.com>
2023-11-08 16:18:22 +0800

    feat: 使用 match case进行匹配

--- temp.py
+++ temp.py
@@ -1,4 +1,11 @@
-valA=12
-valB="ab"
-rawStr=f"{valA}:{valB}"
-print(rawStr)
+li = [1, 2, 3, 4, 5]
+for item in li:
+    match item:
+        case 1:
+            print("11")
+        case 2:
+            print("22")
+        case 3:
+            print("33")
+        case _:
+            print("end")
