e9ce1558666d4c251ccae655d7421290b6b567f7
Zeddy <zrendless@foxmail.com>
2025-01-14 14:39:43 +0800

    feat: 增加摄像头读取

---
+++ 小工具/打开摄像头.py
@@ -0,0 +1,24 @@
+﻿import cv2
+
+# 创建视频捕捉对象
+cap = cv2.VideoCapture()
+cap.open(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # 调整帧的大小
+    # frame = cv2.resize(frame,(640*512))
+    cv2.imshow('frame', frame)
+
+    # 写入调整大小后的帧到输出视频
+    # out.write(resized_frame)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+# 释放资源
+cap.release()
+# out.release()
+cv2.destroyAllWindows()
